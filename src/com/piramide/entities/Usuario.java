48db0d298050ed02b08d1e6fc27bc809da057c2c
fer51mzn <fer51mzn@gmail.com>
2021-01-27 09:17:15 +0100

    Creadas entidades

---
+++ src/com/piramide/entities/Usuario.java
@@ -0,0 +1,12 @@
+package com.piramide.entities;
+
+public class Usuario {
+
+    private String alias;
+    private String passed;
+
+    public Usuario(String alias, String passed) {
+        this.alias = alias;
+        this.passed = passed;
+    }
+}
